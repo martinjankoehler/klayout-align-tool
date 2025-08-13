3a2b37ee73db03e7aca419e0c25570f9fd3fb790
Martin Jan Köhler <info@martinjankoehler.com>
2025-08-13 23:11:30 +0200

    Updated video2gif conversion script

--- doc/convert-mov-to-gif.sh
+++ doc/convert-mov-to-gif.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-VIDEO=klayout-align-screenshot-demo-video.mov
-GIF=klayout-align-screenshot-demo-video.gif
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <input-video> <output-gif>"
+	exit 1
+fi 
+
+VIDEO=$1  #klayout-align-screenshot-demo-video.mov
+GIF=$2   #klayout-align-screenshot-demo-video.gif
 
 ffmpeg -i $VIDEO \
        -vf "fps=15,scale=800:-1:flags=lanczos,palettegen" \
